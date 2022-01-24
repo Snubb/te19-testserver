3aa227aef207009ba8c172e794b57a0249ba7b01
Snubb <oliver.lundqvist@elev.ga.ntig.se>
2022-01-24 11:00:53 +0100

    updated tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -16,9 +16,59 @@ router.get('/', async (req, res, next) => {
     await pool.promise()
         .query('SELECT * FROM tasks')
         .then(([rows, fields]) => {
-            console.log(rows);
-            res.json(rows);
+            res.json({
+                tasks: {
+                    data: rows
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                tasks: {
+                    error: "Cannot retrieve tasks"
+                }
+            });
         });
 });
+
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    console.log(id);
+    if(isNaN(id)) {
+        res.status(400).json({
+            task: {
+                error: "Input a number you fool"
+            }
+        });
+    } else {
+        await pool.promise()
+        .query('SELECT * FROM tasks WHERE id = ' + id)
+        .then(([rows, fields]) => {
+            if(rows.length != 0) {
+                res.json({
+                    tasks: {
+                        data: rows
+                    }
+                });
+            } else {
+                res.json({
+                    tasks: {
+                        error: "ID does not exist"
+                    }
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                tasks: {
+                    error: "Cannot retrieve tasks"
+                }
+            });
+        });
+    }
+});
+
 
 module.exports = router;
